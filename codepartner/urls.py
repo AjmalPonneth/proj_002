17e237c27ea733f9813fdc5ec1a71ae187abcfd0
AjmalPonneth <ponnethajmal@gmail.com>
2021-09-18 23:49:11 +0530

    configured login

--- codepartner/urls.py
+++ codepartner/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LandingView, LoginView, RegisterView
+from .views import LandingView, LoginView, RegisterView, IndexView
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('login', LoginView.as_view(), name='login'),
     path('register', RegisterView.as_view(), name='register'),
+    path('index', IndexView.as_view(), name='index'),
 ]
--- codepartner/views.py
+++ codepartner/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as dj_login
 # Create your views here.
 
 
 class LandingView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'landing.html'
 
 
-class LoginView(TemplateView):
-    template_name = 'login.html'
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(self.request, 'login.html')
+
+    def post(self, request, *args, **kwargs):
+        name = self.request.POST['username']
+        print(name)
+        password = self.request.POST['password']
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            dj_login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('Not Valid!')
 
 
 class RegisterView(TemplateView):
     template_name = 'register.html'
+
+
+class IndexView(TemplateView, View):
+    template_name = 'index.html'
