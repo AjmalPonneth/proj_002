87cd8f4e848ab6d34b620da79453004b94049cda
AjmalPonneth <ponnethajmal@gmail.com>
2021-09-18 23:25:05 +0530

    completed legister template

--- codepartner/urls.py
+++ codepartner/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LandingView, LoginView
+from .views import LandingView, LoginView, RegisterView
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('login', LoginView.as_view(), name='login'),
+    path('register', RegisterView.as_view(), name='register'),
 ]
--- codepartner/views.py
+++ codepartner/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
 # Create your views here.
 
 
