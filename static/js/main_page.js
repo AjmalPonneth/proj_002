f01af6b9c7b517200ef72105869ee6d729cdf0a1
AjmalPonneth <ponnethajmal@gmail.com>
2021-09-29 19:48:13 +0530

    session card completed

--- static/js/main_page.js
+++ static/js/main_page.js
@@ -1,8 +1,6 @@
 window.onload = onPageLoad()
 function onPageLoad() {
-  if (document.getElementById("test").checked == true) {
-    document.getElementById("test").checked = true
-  } else {
-    document.getElementById("test").checked = false
+  if (document.getElementById("check").checked == true) {
+    console.log("hhhhhhhh")
   }
 }
